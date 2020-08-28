561d457292c19b533e58d4302c46147c713dc914
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-28 20:06:34 +0400

    Queue and Stack Implementation is done

---
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/Queue_Collection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structure_and_algorithms.Collections
+{
+    class Queue_Collection
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue("E");
+            q.Enqueue("y");
+            q.Enqueue("a");
+            q.Enqueue("d");
+
+            if (q.Contains("d"))
+                Console.WriteLine("Yes");
+
+            Console.WriteLine(q.Count); //Print number of elements
+            Console.WriteLine(q.Peek());
+
+            while (q.Count > 0)
+                Console.Write(q.Dequeue());
+        }
+    }
+}
---
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/Stack_Collection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structure_and_algorithms.Collections
+{
+    class Stack_Collection
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            Stack<int> stack = new Stack<int>(arr);
+            stack.Push(5);
+            stack.Push(9);
+            stack.Push(7);
+
+            Console.WriteLine(stack.Peek());
+
+            if (stack.Contains(2))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+            Console.WriteLine("Pop the elements from the stack:");
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+        }
+    }
+}
