a984f477af65dbfdcf8b7878eb8e5c787d8766af
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-28 16:03:36 +0400

    Sorted List is added

--- data_structure_and_algorithms/data_structure_and_algorithms/Collections/List_Item.cs
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/List_Item.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace data_structure_and_algorithms.Collections
 {
--- data_structure_and_algorithms/data_structure_and_algorithms/Collections/Sorted_List.cs
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/Sorted_List.cs
@@ -9,9 +9,12 @@ namespace data_structure_and_algorithms.Collections
         {
             SortedList<int, string> sortedList = new SortedList<int, string>();
             sortedList.Add(5, "Moayad");
+            sortedList.Add(99, "bb");
             sortedList.Add(2, "Mohanned");
             sortedList.Add(3, "Mohammed");
+            sortedList.Add(77, "aa");
             sortedList.Add(1, "Eyad");
+            //Console.WriteLine(sortedList[1]); //Print the value of key '1'
             /*var sortedList = new SortedList<int, string>()
             {
                 {5, "Moayad" },
@@ -20,8 +23,17 @@ namespace data_structure_and_algorithms.Collections
                 {1, "Eyad" }
             };*/
 
-            foreach (KeyValuePair<int, string> kvp in sortedList)
-                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
+            sortedList.RemoveAt(5); //Remove by index
+            sortedList.Remove(77);// Remove by key
+
+            /*foreach (KeyValuePair<int, string> item in sortedList)
+                Console.WriteLine("key: {0}, value: {1}", item.Key, item.Value);*/
+            for (int i = 0; i < sortedList.Count; i++)
+                Console.WriteLine("key: {0}, value: {1}", sortedList.Keys[i], sortedList.Values[i]);
+
+            string val;
+            if (sortedList.TryGetValue(3, out val))
+                Console.WriteLine("{0}, {1}", 3, val);
         }
     }
 }
