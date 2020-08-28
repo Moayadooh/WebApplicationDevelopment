c1c51a114db8f9c16cc355d207a3527b5cfa7e8b
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-28 15:36:13 +0400

    Sorted List is in progress

--- data_structure_and_algorithms/data_structure_and_algorithms/Collections/Sorted_List.cs
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/Sorted_List.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace data_structure_and_algorithms.Collections
 {
@@ -6,8 +7,21 @@ namespace data_structure_and_algorithms.Collections
     {
         static void Main(string[] args)
         {
-            int a = 5 + 6;
-            Console.WriteLine(a);
+            SortedList<int, string> sortedList = new SortedList<int, string>();
+            sortedList.Add(5, "Moayad");
+            sortedList.Add(2, "Mohanned");
+            sortedList.Add(3, "Mohammed");
+            sortedList.Add(1, "Eyad");
+            /*var sortedList = new SortedList<int, string>()
+            {
+                {5, "Moayad" },
+                {2, "Mohanned" },
+                {3, "Mohammed" },
+                {1, "Eyad" }
+            };*/
+
+            foreach (KeyValuePair<int, string> kvp in sortedList)
+                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
         }
     }
 }
