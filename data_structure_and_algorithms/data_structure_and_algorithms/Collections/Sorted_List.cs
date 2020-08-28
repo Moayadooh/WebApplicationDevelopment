34b2a94a7446468c5ecbe3c77d63695b6b74a05c
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-28 15:03:42 +0400

    Please Commit

---
+++ data_structure_and_algorithms/data_structure_and_algorithms/Collections/Sorted_List.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace data_structure_and_algorithms.Collections
+{
+    class Sorted_List
+    {
+        static void Main(string[] args)
+        {
+            int a = 5 + 6;
+            Console.WriteLine(a);
+        }
+    }
+}
