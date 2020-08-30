2c98e34da290f4fb7d9423e936883973dc03e1fa
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-30 15:20:40 +0400

    Java Script is done

--- HTML CSS JavaScript/HTML CSS JavaScript/JavaScript.js
+++ HTML CSS JavaScript/HTML CSS JavaScript/JavaScript.js
@@ -1,1 +1,17 @@
-﻿
+﻿function Greet() {
+    msg = document.getElementById("txt1").value;
+    alert(msg);
+}
+
+function ShowHide() {
+    var status = document.getElementById("images").style.display;
+    if (status == "none")
+        status = "block";
+    else
+        status = "none";
+    document.getElementById("images").style.display = status;
+}
+
+function ChangeTitle() {
+    document.getElementById("title1").innerHTML = "Oman";
+}
