367b0a5207490a28e4738a4a9f8115bfd3d8ba7f
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-08-31 12:22:57 +0400

    Some work in JS and HTML is done

--- HTML CSS JavaScript/HTML CSS JavaScript/JavaScript.js
+++ HTML CSS JavaScript/HTML CSS JavaScript/JavaScript.js
@@ -1,6 +1,40 @@
-﻿function Greet() {
-    msg = document.getElementById("txt1").value;
-    alert(msg);
+﻿function Verify() {
+    name = frm.name.value;
+    mobileNo = frm.mobile.value;
+    age = frm.age.value;
+    gender = null;
+    isOK = true;
+
+    if (!isNaN(name)) {
+        document.getElementById("name").style.border = "2px solid red";
+        window.alert("Please enter a valid name!!");
+        isOK = false;
+    }
+    if (!parseInt(mobileNo) || mobileNo.length != 8) {
+        document.getElementById("mobile").style.border = "2px solid red";
+        window.alert("Please enter a valid mobile number!!");
+        isOK = false;
+    }
+    if (isNaN(age)) {
+        document.getElementById("age").style.border = "2px solid red";
+        window.alert("Please enter a valid age!!");
+        isOK = false;
+    }
+    if (document.getElementById("male").checked)
+        gender = frm.gender.value;
+    else if (document.getElementById("female").checked)
+        gender = frm.gender.value;
+    if (gender == null) {
+        window.alert("Please select a gender!!");
+        isOK = false;
+    }
+
+    if (isOK) {
+        document.getElementById("name").style.border = "none";
+        document.getElementById("mobile").style.border = "none";
+        document.getElementById("age").style.border = "none";
+        window.alert("Name is " + name + "\nMobile No is " + mobileNo + "\nAge is " + age + "\nGender is " + gender);
+    }
 }
 
 function ShowHide() {
